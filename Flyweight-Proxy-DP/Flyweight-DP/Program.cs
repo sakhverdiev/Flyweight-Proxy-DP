18a82e4dd132bcafcf81605a0a8aac13fbb09319
Sanan Akhverdiev <120374801+sakhverdiev@users.noreply.github.com>
2023-12-07 10:14:07 +0400

    first commit

---
+++ Flyweight-Proxy-DP/Flyweight-DP/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+interface Shape
+{
+    void draw();
+}
+
+public class Circle : Shape
+{
+    private string color;
+    private int x;
+    private int y;
+    private int radius;
+
+    public Circle(String color)
+    {
+        this.color = color;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public void setRadius(int radius)
+    {
+        this.radius = radius;
+    }
+
+    // override
+    public void draw()
+    {
+        Console.WriteLine("Color : " + color + ", x : " + x + ", y :" + y + ", radius :" + radius);
+    }
+}
+
+public class ShapeFactory
+{
+    private static HashMap circleMap = new HashMap();
+
+    public static Shape getCircle(string color)
+    {
+        Circle circle = (Circle)circleMap.get(color);
+
+        if (circle == null)
+        {
+            circle = new Circle(color);
+            circleMap.put(color, circle);
+            Console.WriteLine("Creating circle of color : " + color);
+        }
+        return circle;
+    }
+}
+
+
+//class Program { 
+//    public static void Main()
+//    {
+//        Shape shape = new Circle();
+//        Console.WriteLine("abc");
+//    }
+//}
---
+++ Flyweight-Proxy-DP/Proxy-DP/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Proxy_DP;
+
+interface Image
+{
+    void display();
+}
+
+
+public class RealImage : Image
+{
+    private string fileName;
+
+    public RealImage(string fileName)
+    {
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+
+    // override
+    public void display()
+    {   
+        Console.WriteLine("Displaying " + fileName);
+    }
+
+    private void loadFromDisk(string fileName)
+    {
+        Console.WriteLine("Loading " + fileName);
+    }
+}
+
+
+public class ProxyImage : Image
+{
+    private RealImage realImage;
+    private string fileName;
+
+    public ProxyImage(string fileName)
+    {
+        this.fileName = fileName;
+    }
+
+
+    // override
+    public void display()
+    {
+        if (realImage == null)
+        {
+            realImage = new RealImage(fileName);
+        }
+        realImage.display();
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Image image = new ProxyImage("abc.jpg");
+
+
+        image.display();
+        Console.WriteLine("image loaded from disk");
+
+
+        image.display();
+        Console.WriteLine("image will not be loaded from disk");
+    }
+}
